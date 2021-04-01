2f67f44f718df67b06c10cfe2a1349c53c120c14
Alexander Shachor <alexshachor@gmail.com>
2021-04-01 15:42:55 +0300

    Comments

--- Core/runMeasurements.py
+++ Core/runMeasurements.py
@@ -6,13 +6,30 @@ from Services.runMeasurementsService import MeasurementsService
 
 
 class RunMeasurements:
+    """*************************************** Description of the Class - RunMeasurements:
+        Gaol: This class is responsible for running all measurements and sending
+        the results data to the server.
+    ***********************************************************************************"""
 
     def __init__(self, measurements, lesson_configuration):
+        """********************************* Description of the Function - __init__:
+                Gaol: The constructor function.
+                Parameters: measurements - List of objects [each object is a measure
+                that needs to be executed and get its results].
+            *************************************************************************"""
+
         # [fd.FaceDetector(),hp(),ot(),sd()]
         self.measurements = measurements
         self.configuration = lesson_configuration
 
     def run(self):
+        """************************************** Description of the Function - run:
+              Gaol: Streaming from the user's camera at regular intervals, for the
+              captured frame all the objects of the measurement are activated - the
+              results are sent to the server.
+              Parameters: self.
+          *************************************************************************"""
+
         measurements_service = MeasurementsService()
 
         try:
