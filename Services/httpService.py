8e5ff688e7c993c78a300b8824fce265fe8c97e3
Alexander Shachor <alexshachor@gmail.com>
2021-04-01 14:29:43 +0300

    1. Add support for post image 2. Renaming

--- Services/httpService.py
+++ Services/httpService.py
@@ -1,3 +1,6 @@
+import base64
+import json
+
 import requests
 from Services import loggerService
 
@@ -12,15 +15,19 @@ def get(url, params):
     try:
         if not url:
             raise ValueError(f'cannot get, url is missing')
-        r = requests.get(url, params)
-        r.raise_for_status()
-        return r.json()
+        response = requests.get(url, params)
+        response.raise_for_status()
+        data = response.json()
+        return data
     except ValueError as e:
         loggerService.get_logger().error(str(e))
         return None
+    except requests.exceptions.RequestException:
+        loggerService.get_logger().error(str(response.text))
+        return None
     except Exception as e:
         loggerService.get_logger().error(
-            f'get call to url: {url} has failed with status: {r.status_code}, due to: {str(e)}')
+            f'get call to url: {url} has failed with status: {response.status_code}, due to: {str(e)}')
         return None
 
 
@@ -40,7 +47,45 @@ def post(url, data):
     except ValueError as e:
         loggerService.get_logger().error(str(e))
         return False
+    except requests.exceptions.RequestException:
+        loggerService.get_logger().error(str(r.text))
+        return False
     except Exception as e:
         loggerService.get_logger().error(
             f'post call to url: {url}, data: {data} has failed with status: {r.status_code}, due to: {str(e)}')
+        return False
+
+
+def post_image_data(url, data, image_file):
+    """
+    post the given data to the given url
+    :param url: post the data to this url
+    :param data: data to send the server
+    :param image_file: image to send along with the data
+    :return: True if succeed and False otherwise
+    """
+    try:
+        if not data or not url or not image_file:
+            raise ValueError(f'cannot post, one of the params is missing.'
+                             f' url: {url}, data: {data}, image: {image_file}')
+
+        with open(image_file, "rb") as f:
+            im_bytes = f.read()
+        im_b64 = base64.b64encode(im_bytes).decode("utf8")
+
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+        payload = json.dumps({"image": im_b64, 'data': data})
+        response = requests.post(url, data=payload, headers=headers)
+        response.raise_for_status()
+        return response.ok
+    except ValueError as e:
+        loggerService.get_logger().error(str(e))
+        return False
+    except requests.exceptions.RequestException:
+        loggerService.get_logger().error(str(response.text))
+        return False
+    except Exception as e:
+        loggerService.get_logger().error(
+            f'post call to url: {url}, data: {data} has failed with status: {response.status_code}, due to: {str(e)}')
         return False
