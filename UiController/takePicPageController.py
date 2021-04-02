e55922104bfbda1995b5c91c6b1da568449a5b1f
ShlomiRosh <shalom053@gmail.com>
2021-04-02 18:21:56 +0300

    Update changes in take pic controller.

--- UiController/takePicPageController.py
+++ UiController/takePicPageController.py
@@ -1,17 +1,20 @@
 from PIL import Image
 from UiController import uiController
 from Measurements import faceRecognition as fr
+from Core import runMeasurements as rm
 import config
 import time
 
 
 def check_recognition(path):
+    measurements = rm.RunMeasurements([fr.FaceRecognition()], None)
     if config.DEBUG:
         time.sleep(2)
         return True
     # TODO - check if image need to be change into frame.
     img = Image.open(path)
-    return fr.FaceRecognition().run(img)
+    face_recognition_res = measurements.run_measurement_processes(img)
+    return face_recognition_res['FaceRecognition']
 
 
 def successes():
