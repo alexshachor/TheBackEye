a3f943f407116c0016b5c043a15600feeab7117e
Alexander Shachor <alexshachor@gmail.com>
2021-04-10 18:51:13 +0300

    Reformat file

--- Core/measurementsResult.py
+++ Core/measurementsResult.py
@@ -1,13 +1,15 @@
 import datetime
 
+
 class MeasurementsResult:
     """
     This class holds the result and current time in order to know what time the measures were taken
     """
+
     def __init__(self, measurements_result):
         """
         initialize the class by setting the result and the current time.
         :param measurements_result: a dictionary which contains all the measurements result.
         """
-        self.result =  measurements_result
+        self.result = measurements_result
         self.time = datetime.datetime.now()
