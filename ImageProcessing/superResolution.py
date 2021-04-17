cb564e8fd0346401e344b4b53e95f585f8f03796
ShlomiRosh <shalom053@gmail.com>
2021-04-17 13:03:51 +0300

    superResolution.py bicubic interpolation func.

--- ImageProcessing/superResolution.py
+++ ImageProcessing/superResolution.py
@@ -15,7 +15,17 @@ class SuperResolution:
         self.model_scale = {'LOW': 3, 'MEDIUM': 4, 'HIGH': 8}
 
     def to_bicubic(self):
-        pass
+        start = time.time()
+        bicubic = cv2.resize(self.image, (self.image.shape[1], self.image.shape[0]),
+                             interpolation=cv2.INTER_CUBIC)
+        end = time.time()
+        if config.DEBUG:
+            print('[INFO] Bicubic interpolation took {:.2f} seconds'.format(end - start))
+            cv2.imshow("Original", self.image)
+            cv2.imshow("Bicubic", bicubic)
+            cv2.imwrite('.\SavedImages\\bicubic.jpg', bicubic)
+            cv2.waitKey(0)
+        return bicubic
 
     def to_super_resolution(self):
         pass
