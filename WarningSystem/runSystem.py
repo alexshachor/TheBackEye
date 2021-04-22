3393ad0a7aea8dcd4c4e743782dd95b02911ddd3
ShlomiRosh <shalom053@gmail.com>
2021-04-22 17:48:56 +0300

    WarningSystem runSystem.py general structure.

---
+++ WarningSystem/runSystem.py
@@ -0,0 +1,24 @@
+import multiprocessing as mp
+from WarningSystem import voiceSystem as vs
+from WarningSystem import flickerSystem as fs
+
+
+class RunSystem:
+
+    def __init__(self, indices_dict):
+        pass
+
+    def __run_warning_system(self):
+        pass
+
+    def __init_failed_indices(self):
+        pass
+
+
+def for_tests_only():
+    pass
+
+
+if __name__ == '__main__':
+    for_tests_only()
+
