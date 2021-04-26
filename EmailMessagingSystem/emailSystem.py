546877552893743a2f0ca41b400fbe9ad8e22f7e
ShlomiRosh <shalom053@gmail.com>
2021-04-26 14:03:32 +0300

    emailSystem.py basic structure.

---
+++ EmailMessagingSystem/emailSystem.py
@@ -0,0 +1,18 @@
+import config
+
+
+class EmailSystem:
+
+    def __init__(self):
+        pass
+
+    def send_email(self, msg, recipient_email):
+        pass
+
+
+def for_tests_only():
+    pass
+
+
+if __name__ == '__main__':
+    for_tests_only()
