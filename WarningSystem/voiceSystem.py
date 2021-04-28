0b765c6a629c876be0850b7e56f423ed741bec4e
ShlomiRosh <shalom053@gmail.com>
2021-04-28 11:46:46 +0300

    voiceSystem.py __init_indices_msgs func complete.

--- WarningSystem/voiceSystem.py
+++ WarningSystem/voiceSystem.py
@@ -18,7 +18,15 @@ class VoiceSystem:
 
     @staticmethod
     def __init_indices_msgs():
-        pass
+        indices_msgs = {
+            'FACEDETECTOR': 'Our system suspects you are not in front of the screen.',
+            'HEADPOSE': 'Our system notes that you are not looking at the screen.',
+            'ONTOP': 'Our system detected that you are running non-lesson related apps.',
+            'OBJECTDETECTOR': 'Our system detected that you are dealing with distracting objects.',
+            'SLEEPDETECTOR': 'Our system suspects you\'m asleep',
+            'FACERECOGNITION': 'Our system suspects that you are not the person in front of the screen.',
+            'MANY_FAILURES': 'Our system updates that you are completely out of learning mode.'
+        }
 
     def __init_msg(self):
         pass
