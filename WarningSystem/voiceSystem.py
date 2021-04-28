b7088917bc0078a72c7eb92761937ce6d21dd6ce
ShlomiRosh <shalom053@gmail.com>
2021-04-28 14:20:02 +0300

    voiceSystem.py comments complete.

--- WarningSystem/voiceSystem.py
+++ WarningSystem/voiceSystem.py
@@ -8,6 +8,11 @@ GENERAL_WARNING = 2
 class VoiceSystem:
 
     def __init__(self, indices_list):
+        """
+        init indices & msgs & the model for text to speech msgs
+        given the teacher an option to to give us unique messages for the student.
+        :param indices_list: list of indices the student failed in
+        """
         self.indices_list = indices_list
         self.indices_msgs = self.__init_indices_msgs() if config.TEACHER_MSGS is None else config.TEACHER_MSGS
         self.msg = ''
@@ -21,6 +26,11 @@ class VoiceSystem:
 
     @staticmethod
     def __init_indices_msgs():
+        """
+        create a dictionary of indices and their appropriate msgs if
+        the student failed in those indices.
+        :return: indices_msgs: a general msgs to the voice system.
+        """
         indices_msgs = {
             'FACEDETECTOR': 'Our system suspects you are not in front of the screen.',
             'HEADPOSE': 'Our system notes that you are not looking at the screen.',
@@ -33,6 +43,9 @@ class VoiceSystem:
         return indices_msgs
 
     def __init_msg(self):
+        """
+        initialize the messages into a one long string for the voice model.
+        """
         if len(self.indices_list) > GENERAL_WARNING:
             self.msg = self.indices_msgs['MANY_FAILURES']
             return
@@ -40,6 +53,10 @@ class VoiceSystem:
             self.msg += self.indices_msgs[i] + ' '
 
     def __run_voice_system(self):
+        """
+        run the voice system & if in debug mode, record the voice system
+        into a mp3 file.
+        """
         self.engine.say(self.msg)
         self.engine.say(self.general_msg)
         self.engine.runAndWait()
@@ -49,6 +66,10 @@ class VoiceSystem:
         self.engine.stop()
 
     def __debug_voice(self):
+        """
+        while in debug mode, print all the details about the
+        voice model.
+        """
         # RATE: getting details of current speaking rate.
         rate = self.engine.getProperty('rate')
         # Printing current voice rate.
@@ -63,12 +84,18 @@ class VoiceSystem:
         print(voice)
 
     def __set_voice(self):
+        """
+        set the voice model properties.
+        """
         self.engine.setProperty('rate', 180)
         self.engine.setProperty('volume', 1)
         self.engine.setProperty('voice', self.engine.getProperty('voices')[not FEMALE].id)
 
 
 def for_tests_only():
+    """
+    this function is used only for tests.
+    """
     indices_list = ['FACEDETECTOR', 'OBJECTDETECTOR', 'SLEEPDETECTOR']
     VoiceSystem(indices_list)
     indices_list = ['FACERECOGNITION', 'OBJECTDETECTOR']
