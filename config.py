2303b54284b99df517adf1c7469fdb69edb4221e
ShlomiRosh <shalom053@gmail.com>
2021-04-28 11:36:11 +0300

    config.py comments.

--- config.py
+++ config.py
@@ -20,10 +20,20 @@ LOG_OPTIONS = {
     'date_format': '%d/%m/%y %H:%M:%S',
     'level': logging.DEBUG
 }
+
+"""
+what program should be on top & what are window name.
+use: in onTop.py
+"""
 DESIRED_PROGRAM = {
     'EXPECTED_ON_TOP': 'zoom.exe',
     'HWND': 'Zoom Meeting'
 }
+
+"""
+all the data about the app.
+use: in uiController.py
+"""
 APP = {
     'WIN_SIZE': '430x550+50+20',
     'TITLE': 'TheBackEye',
@@ -32,9 +42,25 @@ APP = {
     'FONT_OUTPUT': ("Ariel", 10),
     'FONT_MSG': ("Ariel", 10, "bold")
 }
+
+"""
+gui app object.
+use: in uiController.py
+"""
 APPLICATION = None
+
+"""
+the name & id the student enter with to the app.
+use: loginPage & probably in the warning system
+"""
 USER_DATA = {
     'USERNAME': '',
     'ID': ''
 }
+
+"""
+what object we do not allow the student to mess with during lesson
+we probably should receive this objects from the teacher.
+use: in class ObjectDetector
+"""
 PROHIBITED_OBJECTS = ['CELL PHONE']
