5705e2186f3e3c1f04bdcbcc4f21d5ad292143fc
ShlomiRosh <shalom053@gmail.com>
2021-04-28 22:21:44 +0300

    runSystem.py __init__ function complete.

--- WarningSystem/runSystem.py
+++ WarningSystem/runSystem.py
@@ -1,12 +1,21 @@
 import multiprocessing as mp
 from WarningSystem import voiceSystem as vs
 from WarningSystem import flickerSystem as fs
+from WarningSystem import emailWarning as ew
 
 
 class RunSystem:
 
     def __init__(self, indices_dict):
-        pass
+        self.indices_dict = indices_dict
+        self.failed_in = []
+        self.dict_process = {
+            'voice': mp.Process(target=vs.VoiceSystem, args=(self.failed_in,)),
+            'flicker': mp.Process(target=fs.FlickerSystem),
+            'email': mp.Process(target=ew.EmailWarning)
+        }
+        self.__init_failed_indices()
+        self.__run_warning_system()
 
     def __run_warning_system(self):
         pass
