fced4e8077301c73d4875bafdcaba37e5509fe80
ShlomiRosh <shalom053@gmail.com>
2021-04-30 14:04:23 +0300

    emailSystem.py init function complete.

--- EmailMessagingSystem/emailSystem.py
+++ EmailMessagingSystem/emailSystem.py
@@ -4,7 +4,10 @@ import config
 class EmailSystem:
 
     def __init__(self):
-        pass
+        self.sender = config.EMAIL['EMAIL']
+        self.password = config.EMAIL['PASSWORD']
+        self.port = 465
+        self.success = False
 
     def send_email(self, msg, recipient_email):
         pass
