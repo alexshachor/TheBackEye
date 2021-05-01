81a7314e2375bf969cd48a07bf31f4b5f8bedbdb
ShlomiRosh <shalom053@gmail.com>
2021-05-01 12:43:28 +0300

    validationPage.py init function completed.

--- Ui/validationPage.py
+++ Ui/validationPage.py
@@ -14,7 +14,21 @@ FONT_OUTPUT = c.APP['FONT_OUTPUT']
 class ValidationPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        pass
+        tk.Frame.__init__(self, parent)
+        tk.Frame.config(self, bg='black')
+        self.email_img = PhotoImage(file='.\PicUi\\send.png')
+        self.check_img = PhotoImage(file='.\PicUi\\check_pic.png')
+        self.img = PhotoImage(file='.\PicUi\\validationPic.png')
+        self.img1 = PhotoImage(file='.\PicUi\\validationPic1.png')
+        self.email = ''
+        self.count_flg = 0
+        self.invalid_email = None
+        self.invalid_code = None
+        self.pb = None
+        self.validation_controller = vc.ValidationController()
+        self.bg = self.background()
+        self.email_l, self.code_l, self.entry = self.input_output()
+        self.email_b, self.check_b = self.buttons(controller)
 
     def background(self):
         pass
