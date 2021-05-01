c0c38ede13127bf8a1913ec4a5548445ff5564a7
ShlomiRosh <shalom053@gmail.com>
2021-05-01 11:42:02 +0300

    emailWarning.py comments complete.

--- WarningSystem/emailWarning.py
+++ WarningSystem/emailWarning.py
@@ -5,6 +5,9 @@ from EmailMessagingSystem import emailSystem as es
 class EmailWarning:
 
     def __init__(self):
+        """
+        init the email msg & email user
+        """
         self.__msg = """\
             Subject: TheBackEye Warning
 
@@ -17,10 +20,16 @@ class EmailWarning:
         self.__send_msg()
 
     def __send_msg(self):
+        """
+        send the email to the user via EmailSystem
+        """
         es.EmailSystem().send_email(self.__msg, self.__user_email)
 
 
 def for_tests_only():
+    """
+    A test func to this page only.
+    """
     EmailWarning()
 
 
