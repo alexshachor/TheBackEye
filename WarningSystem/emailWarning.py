b18c5a4e34112a17d2c1ca5cd40841f5d25bbd14
ShlomiRosh <shalom053@gmail.com>
2021-05-01 11:22:21 +0300

    emailWarning.py init function complete.

--- WarningSystem/emailWarning.py
+++ WarningSystem/emailWarning.py
@@ -5,7 +5,16 @@ from EmailMessagingSystem import emailSystem as es
 class EmailWarning:
 
     def __init__(self):
-        pass
+        self.__msg = """\
+            Subject: TheBackEye Warning
+
+            Our system detected that you are messing with your cell phone,
+            please return to learning mode.
+
+            TheBackEye Team
+            """
+        self.__user_email = config.EMAIL['USER_EMAIL']
+        self.__send_msg()
 
     def __send_msg(self):
         pass
