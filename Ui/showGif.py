a357a1dfa9e17a3f13ea5a2eddce68338ab26a45
ShlomiRosh <shalom053@gmail.com>
2021-05-13 19:30:06 +0300

    showGif.py show function complete.

--- Ui/showGif.py
+++ Ui/showGif.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 from itertools import count, cycle
+from Services import loggerService as ls
 
 
 class ShowGif(tk.Label):
@@ -10,8 +11,25 @@ class ShowGif(tk.Label):
         self.frames = None
         self.delay = None
 
-    def load(self, root):
-        pass
+    def show(self, root):
+        if isinstance(root, str):
+            root = Image.open(root)
+        frames_img = []
+        try:
+            for i in count(1):
+                frames_img.append(ImageTk.PhotoImage(root.copy()))
+                root.seek(i)
+        except EOFError as e:
+            ls.get_logger().error(f'failed to load the picture, due to: {str(e)}')
+        self.frames = cycle(frames_img)
+        try:
+            self.delay = root.info['duration']
+        except:
+            self.delay = 100
+        if len(frames_img) == 1:
+            self.config(image=next(self.frames))
+        else:
+            self.show_next_frame()
 
     def stop(self):
         self.config(image=None)
