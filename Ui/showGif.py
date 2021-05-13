266d771e816d6f26003c9ec4a90e6c74f11a961c
ShlomiRosh <shalom053@gmail.com>
2021-05-13 18:44:20 +0300

    showGif.py main structure completed.

---
+++ Ui/showGif.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from itertools import count, cycle
+
+
+class ShowGif(tk.Label):
+
+    def __init__(self, **kw):
+        pass
+
+    def load(self, root):
+        pass
+
+    def unload(self):
+        pass
+
+    def show_next_frame(self):
+        pass
+
+
+def for_tests_only():
+    pass
+
+
+if __name__ == "__main__":
+    for_tests_only()
