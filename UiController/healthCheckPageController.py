394e1bf339685838f9565783f1b741ef2c3282c7
ShlomiRosh <shalom053@gmail.com>
2021-05-24 12:30:01 +0300

    healthCheckPageController.py general structure.

---
+++ UiController/healthCheckPageController.py
@@ -0,0 +1,23 @@
+import config
+
+
+class HealthCheckPageController:
+
+    def __init__(self):
+        pass
+
+    def get_health_map(self):
+        pass
+
+    def is_ready(self):
+        pass
+
+
+def health_list_tests():
+    pass
+
+
+if __name__ == '__main__':
+    x = HealthCheckPageController()
+    x.get_health_map()
+    print(x.is_ready())
