f44770d135718864e606ee9ed3723d4ca875d5dc
ShlomiRosh <shalom053@gmail.com>
2021-05-25 18:21:17 +0300

    healthCheckPageController.py check if the program ready to run & create list for testing functions completed.

--- UiController/healthCheckPageController.py
+++ UiController/healthCheckPageController.py
@@ -1,22 +1,27 @@
 import config
+from Services import healthChecksService as hcs
 
 
 class HealthCheckPageController:
 
     def __init__(self):
         self.ready = True
-        self.health_list = health_list_tests()
+        self.health_list = health_list_tests() if config.DEBUG else hcs.run_health_checks()
         self.health_dict = {}
 
     def get_health_map(self):
         pass
 
     def is_ready(self):
-        pass
+        for key, val in self.health_dict.items():
+            if not val:
+                self.ready = False
+        return self.ready
 
 
 def health_list_tests():
-    pass
+    return [{'is_zoom_installed': True}, {'is_manycam_installed': True},
+            {'is_manycam_running': True}, {'is_alive': True}, {'camera_source': True}]
 
 
 if __name__ == '__main__':
