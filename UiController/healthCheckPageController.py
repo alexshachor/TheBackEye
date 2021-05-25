3deed075fd6bc649a7834e790c118e6e75c960fd
ShlomiRosh <shalom053@gmail.com>
2021-05-25 18:37:42 +0300

    healthCheckPageController.py function comments complete.

--- UiController/healthCheckPageController.py
+++ UiController/healthCheckPageController.py
@@ -5,11 +5,18 @@ from Services import healthChecksService as hcs
 class HealthCheckPageController:
 
     def __init__(self):
+        """
+        init function for the status of the program and the health list.
+        """
         self.ready = True
         self.health_list = health_list_tests() if config.DEBUG else hcs.run_health_checks()
         self.health_dict = {}
 
     def get_health_map(self):
+        """
+        create a health map from the health list.
+        :return: self.health_dict: health map for each component what is status
+        """
         for i in range(len(self.health_list)):
             val = self.health_list[i]
             try:
@@ -22,6 +29,10 @@ class HealthCheckPageController:
         return self.health_dict
 
     def is_ready(self):
+        """
+        check if all health components are ready to run.
+        :return: self.ready: a boolean indicating if the process is ready or not
+        """
         for key, val in self.health_dict.items():
             if not val:
                 self.ready = False
@@ -29,6 +40,10 @@ class HealthCheckPageController:
 
 
 def health_list_tests():
+    """
+    function for tests.
+    :return: a list that mimics the operation of the service get health.
+    """
     return [{'is_zoom_installed': True}, {'is_manycam_installed': True},
             {'is_manycam_running': True}, {'is_alive': True}, {'camera_source': True}]
 
