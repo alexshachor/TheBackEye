e1debf9714fe53649cd04785d049fe476c2605a4
ShlomiRosh <shalom053@gmail.com>
2021-05-26 18:51:54 +0300

    faceTraining.py structure complete.

---
+++ Measurements/FaceRecognition/faceTraining.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from PIL import Image
+import config
+
+
+class FaceTraining:
+
+    def __init__(self):
+        pass
+
+    # function to get the images and label data
+    def __get_images_labels(self):
+        pass
+
+    def __train(self):
+        pass
+
+
+def for_tests_only():
+    pass
+
+
+if __name__ == "__main__":
+    for_tests_only()
