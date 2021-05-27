f878e2a96282dbae1d1f3390d441151f24426ff0
ShlomiRosh <shalom053@gmail.com>
2021-05-27 15:59:14 +0300

    faceTraining.py init function complete.

--- Measurements/FaceRecognition/faceTraining.py
+++ Measurements/FaceRecognition/faceTraining.py
@@ -7,7 +7,14 @@ import config
 class FaceTraining:
 
     def __init__(self):
-        pass
+        self.recognizer = cv2.face.LBPHFaceRecognizer_create()
+        self.detector = cv2.CascadeClassifier('.\Models\haarcascade_frontalface_default.xml')
+        self.image_paths = ['.\Images\\1.jpg', '.\Images\\2.jpg', '.\Images\\3.jpg',
+                            '.\Images\\4.jpg', '.\Images\\5.jpg']
+        print('[INFO] Training faces. It will take a few seconds.') if config.DEBUG else None
+        self.faces, self.ids = self.__get_images_labels()
+        self.__train()
+        print('[INFO] faces trained. Exiting Program') if config.DEBUG else None
 
     # function to get the images and label data
     def __get_images_labels(self):
