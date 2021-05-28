d9c7875c7157309b7d5878e82c19e7929782c45c
ShlomiRosh <shalom053@gmail.com>
2021-05-28 18:15:11 +0300

    thanksPage.py add timer in the show time func.

--- Ui/thanksPage.py
+++ Ui/thanksPage.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter as tk
 from UiController import takePicPageController as tp
+from UiController import thanksPageController as tpc
 import config as c
 import time
 import threading
@@ -17,6 +18,9 @@ class TanksPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.img = PhotoImage(file='.\PicUi\\tanks_background.png')
+        self.hour = StringVar()
+        self.minute = StringVar()
+        self.second = StringVar()
         self.background()
 
     def background(self):
@@ -32,11 +36,27 @@ class TanksPage(tk.Frame):
         x.setDaemon(True)
         x.start()
 
-    @staticmethod
-    def show_time():
+    def show_time(self):
         """
         Show msg time.
         """
-        time.sleep(SHOW_MSG_TIME)
+        hour, minute = tpc.get_time_remaining()
+        if hour == 'X':
+            time.sleep(5)
+            tp.successes()
+            return
+        self.hour.set(hour)
+        self.minute.set(minute)
+        self.second.set('00')
+        label = tk.Label(self, text='The lesson will begin in:'
+                         , bg='black', bd=0, fg='green', font=FONT_MSG)
+        label.place(bordermode=OUTSIDE, x=60, y=420)
+        hour_entry = Entry(self, width=3, font=("Arial", 18, ""), textvariable=self.hour)
+        hour_entry.place(x=60, y=470)
+        minute_entry = Entry(self, width=3, font=("Arial", 18, ""), textvariable=self.minute)
+        minute_entry.place(x=110, y=470)
+        second_entry = Entry(self, width=3, font=("Arial", 18, ""), textvariable=self.second)
+        second_entry.place(x=160, y=470)
+        self.submit()
         tp.successes()
 
