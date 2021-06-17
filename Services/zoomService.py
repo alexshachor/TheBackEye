11671043cee51b0328399771fed036ca9114f824
ShlomiRosh <shalom053@gmail.com>
2021-06-17 17:49:19 +0300

    zoomService.py add basic structure.

---
+++ Services/zoomService.py
@@ -0,0 +1,26 @@
+import os
+import platform
+
+
+class ZoomService:
+
+    def __init__(self, link):
+        pass
+
+    def join(self):
+        pass
+
+    @staticmethod
+    def quit():
+        pass
+
+    def __decode_link(self):
+        pass
+
+
+def for_tests_only():
+    pass
+
+
+if __name__ == '__main__':
+    for_tests_only()
