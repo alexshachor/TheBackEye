220efbf8bdddb911017478fe2252118a392093f2
Alexander Shachor <alexshachor@gmail.com>
2021-06-21 21:26:10 +0300

    bug fixes

--- Core/studentManager.py
+++ Core/studentManager.py
@@ -1,33 +1,34 @@
 import config
 from Services import httpService
 
+student = None
 
-class StudentManager():
+
+class StudentManager:
     """
     This class manages the student details that come from remote.
     """
-    student = None
 
-    def get_student(self, password=None):
+    @staticmethod
+    def get_student(password=None):
         """
         Get specific student from remote
-        :param params: params for the GET request (e.g: password)
-        :return: configuration dictionary
+        :param password: the student's password
+        :return: student details dictionary
         """
+        global student
         if password:
             url = config.URLS['get_student'] + password
             result = httpService.get(url)
             if result:
-                self.student = result.json()
+                student = result.json()
 
-        return self.student
+        return student
+
+
 
 
 if __name__ == '__main__':
-    sm = StudentManager()
-    x = sm.get_student("123456789")
-    print(sm.get_student())
-    print(sm.student)
-    sm1 = StudentManager()
-    print(sm1.student)
-    print(sm1.get_student())
+    res = StudentManager.get_student("123456789")
+    if res:
+        print(StudentManager.get_student())
